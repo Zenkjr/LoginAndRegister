7c669b5fb867d2c339ef3bb97a13d06133405b96
Zenkjr <kurohimer@gmail.com>
2018-10-21 23:39:06 +0700

    login and register

---
+++ WebApplication7/WebApplication7/Areas/Identity/Data/WebApplication7User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication7.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the WebApplication7User class
+    public class WebApplication7User : IdentityUser
+    {
+        [PersonalData]
+        public string Name { get; set; }
+        [PersonalData]
+        public DateTime Birthday { get; set; }
+    }
+}
---
+++ WebApplication7/WebApplication7/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplication7.Areas.Identity.Data;
+using WebApplication7.Models;
+
+[assembly: HostingStartup(typeof(WebApplication7.Areas.Identity.IdentityHostingStartup))]
+namespace WebApplication7.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<WebApplication7Context>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("WebApplication7ContextConnection")));
+
+                services.AddDefaultIdentity<WebApplication7User>()
+                    .AddEntityFrameworkStores<WebApplication7Context>();
+            });
+        }
+    }
+}
